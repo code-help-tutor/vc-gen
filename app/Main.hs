e74409ce9039701d816c99425b8d1f539a4ed8ea
Code Help Tutor <1823890830@qq.com>
2024-04-10 06:04:33 +0000

    WeChat cstutorcs vc gen

---
+++ app/Main.hs
@@ -0,0 +1,29 @@
+WeChat: cstutorcs
+QQ: 749389476
+Email: tutorcs@163.com
+module Main 
+  ( main
+  ) where
+
+import System.Exit
+import Config
+import Parse
+import Nano
+
+resultExit :: [Bool] -> ExitCode
+resultExit rs | and rs = ExitSuccess
+resultExit _ = ExitFailure 1
+
+verify :: FilePath -> IO Bool
+verify path = do
+  prog <- nano path
+  prog' <- maybe (fail "Unsupported Javascript in source") return prog
+  valid <- check prog'
+  putStrLn $ "'" <> path <> "' is valid: " <> show valid
+  return valid
+
+main :: IO ()
+main = do
+  cfg <- readConfig
+  rs <- mapM verify $ files cfg
+  exitWith $ resultExit rs
